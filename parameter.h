662b6422d90299e8696e68bdf154313bafd1f078
Heewon Chung <tyler.hchung@gmail.com>
2018-01-19 16:02:06 +0900

    recommended parameter

---
+++ parameter.h
@@ -0,0 +1,11 @@
+#ifndef PARAMETER_H
+#define PARAMETER_H
+
+#include <cstdint>
+
+uint16_t degree             = 256;
+uint16_t dimension          = 3;
+uint16_t coeffModulus       = 2048;
+uint16_t roundingModulus    = 512;
+
+#endif
